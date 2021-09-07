9f87c3bb9575039dd8bcd1a85a8db6ee7c2a1af3
halinh <halinh@localhost>
2021-09-07 18:21:19 +0700

    Test commit 001

---
+++ Test Suites/Get hierarchy branch data.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get hierarchy branch data</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c325c942-889f-4e37-a29b-3d4f0153208f</testSuiteGuid>
+   <testCaseLink>
+      <guid>472fb0d7-0365-44ed-95cb-e2b7f3436df6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Log in Genetica app successfully</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c2ebef6f-c369-4289-a339-36f14249983d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get hierarchy branches</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
